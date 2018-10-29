YaroST12/MATA_Kernel

qdsp6v2: adjust drivers for Mata
After Pie tag was released CAF added functions
for checking fw version that are not supported
by out DSP.

And kernel tell us about it by spamming:

[10186.137518] q6core_get_service_version: Failed to get service size for service id 7
with error -95
[10186.141517] q6core_get_service_version: Failed to get service size for service id 8
with error -95
[10186.151816] q6core_get_service_version: Failed to get service size for service id 7
with error -95
[10254.278514] q6core_get_service_version: Failed to get service size for service id 7
with error -95
[10254.282274] q6core_get_service_version: Failed to get service size for service id 8
with error -95
[10254.292154] q6core_get_service_version: Failed to get service size for service id 7
with error -95
[10294.549313] q6core_get_service_version: Failed to get service size for service id 7
with error -95
[10294.553506] q6core_get_service_version: Failed to get service size for service id 8
with error -95
[10294.563891] q6core_get_service_version: Failed to get service size for service id 7
with error -95

This results in certain audio apps getting focked up
after system suspends and then goes back online.

Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

sound/soc/msm/qdsp6v2/q6core.c
@@ -311,6 +311,7 @@ struct cal_block_data *cal_utils_get_cal_block_by_key(
 	return NULL;
 }
 
+#ifndef CONFIG_BOARD_MATA
 static int q6core_send_get_avcs_fwk_ver_cmd(void)
 {
 	struct apr_hdr avcs_ver_cmd;
@@ -471,6 +472,13 @@ done:
 }
 EXPORT_SYMBOL(q6core_get_fwk_version_size);
 
+#else
+int q6core_get_service_version(uint32_t service_id,
+			       struct avcs_fwk_ver_info *ver_info,
+			       size_t size) { return -1; }
+size_t q6core_get_fwk_version_size(uint32_t service_id)
+	{ return -1; }
+#endif
 int32_t core_set_license(uint32_t key, uint32_t module_id)
 {
 	struct avcs_cmd_set_license *cmd_setl = NULL;

mSALDANHAf/Z2_Kernel

sound/soc/msm/qdsp6v2/q6core.c
@@ -311,6 +311,7 @@ struct cal_block_data *cal_utils_get_cal_block_by_key(
 	return NULL;
 }
 
+#ifndef CONFIG_MACH_ZUK
 static int q6core_send_get_avcs_fwk_ver_cmd(void)
 {
 	struct apr_hdr avcs_ver_cmd;
@@ -471,6 +472,13 @@ done:
 }
 EXPORT_SYMBOL(q6core_get_fwk_version_size);
 
+#else
+int q6core_get_service_version(uint32_t service_id,
+			       struct avcs_fwk_ver_info *ver_info,
+			       size_t size) { return -1; }
+size_t q6core_get_fwk_version_size(uint32_t service_id)
+	{ return -1; }
+#endif
 int32_t core_set_license(uint32_t key, uint32_t module_id)
 {
 	struct avcs_cmd_set_license *cmd_setl = NULL;